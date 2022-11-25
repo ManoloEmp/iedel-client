4c1c16ed7ccb812e4329db7c16a59cc690005519
Manolo Saen <manolosaenzok79@gmail.com>
2022-11-25 18:58:36 -0500

    update contact link

--- src/components/header.js
+++ src/components/header.js
@@ -93,16 +93,16 @@ export default function Header(props) {
             spacing={6}
             pt={{ base: "16px" }}
           >
-            <NavLink to={"https://www.facebook.com/instelena/about"}>
-              <Button
-                as={"a"}
-                fontSize={"md"}
-                fontWeight={600}
-                variant={"link"}
-              >
-                <Text color={"brand.green-core"} variant="small">Contacto</Text>
-              </Button>
-            </NavLink>
+            <Button
+              as={"a"}
+              fontSize={"md"}
+              fontWeight={600}
+              variant={"link"}
+              href={"https://www.facebook.com/instelena/about"}
+            >
+              <Text color={"brand.green-core"} variant="small">Contacto</Text>
+            </Button>
+
             {
               /*<Button
               display={{ base: "none", md: "inline-flex" }}
